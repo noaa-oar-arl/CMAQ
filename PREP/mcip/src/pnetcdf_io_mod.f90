22a120ccdfef8de1b8de8baa2f8249264a96d1e5
drnimbusrain <dr.nimbusrain@gmail.com>
2020-03-13 01:24:37 +0000

    Updated MPI

--- PREP/mcip/src/pnetcdf_io_mod.f90
+++ PREP/mcip/src/pnetcdf_io_mod.f90
@@ -94,8 +94,8 @@ SUBROUTINE get_var_3d_real_cdf (cdfid, var, dum3d, it, rcode)
   rcode = nf90_inq_varid (cdfid, var, id_data)
   IF ( rcode /= nf90_noerr ) RETURN
 
-  rcode = nf90_get_var (cdfid, id_data, dum3d, start=(/(my_rank*nx/p)+1,1,1,it/),  &
-                        count=(/((my_rank+1)*nx/p),ny,nz,1/))
+  rcode = nf90_get_var (cdfid, id_data, dum3d, start=(/(my_rank*nx/p)+1,(my_rank*ny/p)+1,1,it/),  &
+                        count=(/(nx/p),(ny/p),nz,1/))
   IF ( rcode /= nf90_noerr ) then
    print*,'read error ',cdfid,var
    print*,'nx,ny,nz=',nx,ny,nz
@@ -138,8 +138,8 @@ SUBROUTINE get_var_3d_int_cdf (cdfid, var, idum3d, it, rcode)
   rcode = nf90_inq_varid (cdfid, var, id_data)
   IF ( rcode /= nf90_noerr ) RETURN
 
-  rcode = nf90_get_var (cdfid, id_data, idum3d, start=(/(my_rank*nx/p)+1,1,1,it/),  &
-                        count=(/((my_rank+1)*nx/p),ny,nz,1/))
+  rcode = nf90_get_var (cdfid, id_data, idum3d, start=(/(my_rank*nx/p)+1,(my_rank*ny/p)+1,1,it/),  &
+                        count=(/(nx/p),(ny/p),nz,1/))
 
 END SUBROUTINE get_var_3d_int_cdf
 
@@ -177,8 +177,8 @@ SUBROUTINE get_var_2d_real_cdf (cdfid, var, dum2d, it, rcode)
    print*,'can not find variable ',trim(var)
    RETURN
   endif
-  rcode = nf90_get_var (cdfid, id_data, dum2d, start=(/(my_rank*nx/p)+1,1,it/),  &
-                        count=(/((my_rank+1)*nx/p),ny,1/))
+  rcode = nf90_get_var (cdfid, id_data, dum2d, start=(/(my_rank*nx/p)+1,(my_rank*ny/p)+1,it/),  &
+                        count=(/(nx/p),(ny/p),1/))
   IF ( rcode /= nf90_noerr ) print*,'read error for ',trim(var),&
    ' id_data,it,nx,ny=',id_data,it,nx,ny
 
@@ -216,8 +216,8 @@ SUBROUTINE get_var_2d_int_cdf (cdfid, var, idum2d, it, rcode)
   rcode = nf90_inq_varid (cdfid, var, id_data)
   IF ( rcode /= nf90_noerr ) RETURN
 
-  rcode = nf90_get_var (cdfid, id_data, idum2d, start=(/(my_rank*nx/p)+1,1,it/),  &
-                        count=(/((my_rank+1)*nx/p),ny,1/))
+  rcode = nf90_get_var (cdfid, id_data, idum2d, start=(/(my_rank*nx/p)+1,(my_rank*ny/p)+1,it/),  &
+                        count=(/(nx/p),(ny/p),1/))
 
 END SUBROUTINE get_var_2d_int_cdf
 
